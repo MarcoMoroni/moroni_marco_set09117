e5aa77468db7b19dbe8126b7105487a412a5caae
Marco Moroni <marcomoroni@icloud.com>
2017-10-07 15:33:56 +0100

    Add piece class

--- Checkers.py
+++ Checkers.py
@@ -2,6 +2,11 @@
 # CHECKERS #
 ############
 
+from enum import Enum
+class PieceRank(Enum):
+    MAN = 0
+    KING = 1
+
 # board
 boardDimension = 8
 emptySquareChar = '.'
@@ -19,3 +24,20 @@ def printBoard():
 class Player:
     def __init__(self, symbol):
         self.symbol = symbol
+
+
+# Piece
+class Piece:
+    rank = PieceRank.MAN
+    
+    def __init__(self, player):
+        self.player = player
+    
+    def becomesKing(self):
+        self.rank = PieceRank.KING
+
+
+p = Piece("A")
+print(p.rank)
+p.becomesKing()
+print(p.rank)
